227f591b6e88a47cc888e5b193a309761c6730f9
Marc Schmidt <m_schmidt94@web.de>
2025-08-16 13:56:19 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/endless-horde/',
+  base: './',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 })
